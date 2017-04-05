22c78c0b23b6e7c8ac63669105b7a40eeb5ae526
Viliam Durina <viliam@hazelcast.com>
2017-04-05 13:38:45 +0200

    Checkstyle fix

--- hazelcast-jet-core/src/main/java/com/hazelcast/jet/impl/connector/ReadFileP.java
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/impl/connector/ReadFileP.java
@@ -30,7 +30,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.StreamSupport;
