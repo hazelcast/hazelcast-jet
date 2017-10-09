687d1f3db526ba859c9b3b688c76a7bbfb9a97f0
Viliam Durina <viliam@hazelcast.com>
2017-10-09 11:46:42 +0200

    Add note on ringbuffer overflow to javadoc

--- hazelcast-jet-core/src/main/java/com/hazelcast/jet/Sources.java
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/Sources.java
@@ -151,7 +151,9 @@ public final class Sources {
      * subset of all local partitions to stream.
      * <p>
      * In order to stream from a map, event-journal should be configured.
-     * See {@link com.hazelcast.config.EventJournalConfig}.
+     * See {@link com.hazelcast.config.EventJournalConfig}. The event journal
+     * is backed by a fixed capacity ring buffer. If the capacity overflows,
+     * some events can be missed - make sure to configure that too.
      * <p>
      * Journal offset is saved to the state snapshot. If the job restarts,
      * emission starts at the saved offset, giving exactly-once guarantee.
@@ -230,7 +232,9 @@ public final class Sources {
      * events at the source.
      * <p>
      * In order to stream from a map, event-journal should be configured.
-     * Please see {@link com.hazelcast.config.EventJournalConfig}
+     * See {@link com.hazelcast.config.EventJournalConfig}. The event journal
+     * is backed by a fixed capacity ring buffer. If the capacity overflows,
+     * some events can be missed - make sure to configure that too.
      * <p>
      * Journal offset is saved to the state snapshot. If the job restarts,
      * emission starts at the saved offset, giving exactly-once guarantee.
@@ -296,7 +300,9 @@ public final class Sources {
      * subset of all local partitions to stream.
      * <p>
      * In order to stream from a cache, event-journal should be configured.
-     * Please see {@link com.hazelcast.config.EventJournalConfig}.
+     * See {@link com.hazelcast.config.EventJournalConfig}. The event journal
+     * is backed by a fixed capacity ring buffer. If the capacity overflows,
+     * some events can be missed - make sure to configure that too.
      * <p>
      * Journal offset is saved to the state snapshot. If the job restarts,
      * emission starts at the saved offset, giving exactly-once guarantee.
@@ -360,7 +366,9 @@ public final class Sources {
      * events at the source.
      * <p>
      * In order to stream from a cache, event-journal should be configured.
-     * Please see {@link com.hazelcast.config.EventJournalConfig}.
+     * See {@link com.hazelcast.config.EventJournalConfig}. The event journal
+     * is backed by a fixed capacity ring buffer. If the capacity overflows,
+     * some events can be missed - make sure to configure that too.
      * <p>
      * Journal offset is saved to the state snapshot. If the job restarts,
      * emission starts at the saved offset, giving exactly-once guarantee.
