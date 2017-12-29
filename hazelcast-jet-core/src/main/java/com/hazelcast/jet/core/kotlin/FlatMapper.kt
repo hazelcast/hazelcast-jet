b127aee6eaa15908720122c0e7a5cb89daa4c756
Marko Topolnik <marko@hazelcast.com>
2017-12-29 17:43:28 +0100

    Demo

---
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/core/kotlin/FlatMapper.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.jet.core.kotlin
+
+import com.hazelcast.jet.Traverser
+import java.util.function.Function
+
+class FlatMapper<in T, out R>(
+        private val mapper: Function<in T, out Traverser<out R>>
+) : AbstractProcessorK() {
+    override var isCooperative = true
+
+    suspend override fun process(ordinal: Int, item: Any) {
+    }
+}
