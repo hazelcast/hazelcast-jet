14a6f6edc3c0ccf3b5a769f7277420d599a903ff
Marko Topolnik <marko@hazelcast.com>
2018-03-02 14:22:40 +0100

    Update Javodoc

--- hazelcast-jet-core/src/main/java/com/hazelcast/jet/pipeline/ContextFactory.java
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/pipeline/ContextFactory.java
@@ -28,11 +28,13 @@ import static com.hazelcast.jet.function.DistributedFunctions.noopConsumer;
 /**
  * A holder of functions needed to create and destroy a context object that
  * can be used in the Pipeline API to give the transforming functions (map,
- * filter, flatMap) access to the local {@code JetInstance}. Among others,
- * this gives them the ability to interact with IMDG data structures such
- * as {@code IMap} and {@code ReplicatedMap}.
+ * filter, flatMap) access to some resource that would be too expensive to
+ * acquire-release on each function call. It also provides access to the
+ * local {@code JetInstance}. Among others, this gives you the ability to
+ * interact with IMDG data structures such as {@code IMap} and {@code
+ * ReplicatedMap} during the transformation.
  * <p>
- * These are the components:
+ * You can use the context factory from these Pipeline API methods:
  * <ul>
  *     <li>{@link GeneralStage#mapUsingContext}
  *     <li>{@link GeneralStage#filterUsingContext}
@@ -52,25 +54,27 @@ public final class ContextFactory<C> implements Serializable {
 
     private final DistributedFunction<JetInstance, ? extends C> createFn;
     private final DistributedConsumer<? super C> destroyFn;
-    private final boolean cooperative;
-    private final boolean shareLocally;
+    private final boolean isCooperative;
+    private final boolean isSharedLocally;
 
     private ContextFactory(
             DistributedFunction<JetInstance, ? extends C> createFn,
             DistributedConsumer<? super C> destroyFn,
-            boolean cooperative, boolean shareLocally) {
+            boolean isCooperative,
+            boolean isSharedLocally
+    ) {
         this.createFn = createFn;
         this.destroyFn = destroyFn;
-        this.cooperative = cooperative;
-        this.shareLocally = shareLocally;
+        this.isCooperative = isCooperative;
+        this.isSharedLocally = isSharedLocally;
     }
 
     /**
-     * Creates new {@link ContextFactory} with given create function.
+     * Creates a new {@link ContextFactory} with the given create-function.
      *
      * @param createContextFn the function to create new context object, given a JetInstance
      * @param <C> the user-defined context object type
-     * @return a new instance
+     * @return a new factory instance
      */
     @Nonnull
     public static <C> ContextFactory<C> withCreateFn(
@@ -80,50 +84,50 @@ public final class ContextFactory<C> implements Serializable {
     }
 
     /**
-     * Returns a copy of this {@link ContextFactory} with destroy function
+     * Returns a copy of this {@link ContextFactory} with the destory-function
      * replaced with the given function.
      *
      * @param destroyFn the function to destroy user-defined context. It will be called
      *                  when the job finishes
-     * @return a new instance with destroy function replaced
+     * @return a copy of this factory with the supplied destroy-function
      */
     @Nonnull
     public ContextFactory<C> withDestroyFn(@Nonnull DistributedConsumer<? super C> destroyFn) {
-        return new ContextFactory<>(createFn, destroyFn, cooperative, shareLocally);
+        return new ContextFactory<>(createFn, destroyFn, isCooperative, isSharedLocally);
     }
 
     /**
-     * Returns a copy of this {@link ContextFactory} with <em>cooperative</em>
-     * flag set to {@code false}. Context factory is cooperative by default.
-     * Call this method if the calls to context are non-cooperative.
+     * Returns a copy of this {@link ContextFactory} with the <em>isCooperative</em>
+     * flag set to {@code false}. The context factory is cooperative by default.
+     * Call this method if any of the calls to the methods in the context may block
+     * or otherwise take long to complete.
      * <p>
-     * The contract of <em>cooperative processing</em> is described {@link
+     * The contract of <em>cooperative multithreading</em> is described {@link
      * com.hazelcast.jet.core.Processor#isCooperative() here}.
      *
-     * @return a new instance with <em>cooperative</em> flag un-set.
+     * @return a copy of this factory with the {@code isCooperative} flag set to {@code false}.
      */
     @Nonnull
     public ContextFactory<C> nonCooperative() {
-        return new ContextFactory<>(createFn, destroyFn, false, shareLocally);
+        return new ContextFactory<>(createFn, destroyFn, false, isSharedLocally);
     }
 
     /**
-     * Returns a copy of this {@link ContextFactory} with <em>share-locally</em>
-     * flag set to {@code true}. By default, context object is not shared, each
-     * parallel processor gets its own instance. If the context object is large
-     * or takes long to initialize, it might be better to share it. If you
-     * enable sharing, the context object will be used from multiple threads -
-     * make sure that it is  <u>thread-safe</u>.
+     * Returns a copy of this {@link ContextFactory} with the <em>shareLocally</em>
+     * flag set to {@code true}. By default the context object is not shared so each
+     * parallel processor gets its own instance. If you enable sharing, the context
+     * object will be used from multiple threads &mdash; make sure that it is
+     * <em>thread-safe</em>.
      *
-     * @return a new instance with <em>share-locally</em> flag set.
+     * @return a copy of this factory with the {@code isSharedLocally} flag set.
      */
     @Nonnull
     public ContextFactory<C> shareLocally() {
-        return new ContextFactory<>(createFn, destroyFn, cooperative, true);
+        return new ContextFactory<>(createFn, destroyFn, isCooperative, true);
     }
 
     /**
-     * Returns the create function.
+     * Returns the create-function.
      */
     @Nonnull
     public DistributedFunction<JetInstance, ? extends C> createFn() {
@@ -131,7 +135,7 @@ public final class ContextFactory<C> implements Serializable {
     }
 
     /**
-     * Returns the destroy function.
+     * Returns the destroy-function.
      */
     @Nonnull
     public DistributedConsumer<? super C> destroyFn() {
@@ -139,16 +143,16 @@ public final class ContextFactory<C> implements Serializable {
     }
 
     /**
-     * Returns the cooperative flag.
+     * Returns the {@code isCooperative} flag.
      */
     public boolean isCooperative() {
-        return cooperative;
+        return isCooperative;
     }
 
     /**
-     * Returns the share-locally flag.
+     * Returns the {@code isSharedLocally} flag.
      */
     public boolean isSharedLocally() {
-        return shareLocally;
+        return isSharedLocally;
     }
 }
