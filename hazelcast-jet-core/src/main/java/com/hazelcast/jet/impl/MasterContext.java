af31657f10712c71e5c181c67dc1de66feae6f0c
Viliam Durina <viliam-durina@users.noreply.github.com>
2018-03-23 14:04:56 +0100

    Fix deserialization error reporting from master
    Fixes #786

--- hazelcast-jet-core/src/main/java/com/hazelcast/jet/impl/MasterContext.java
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/impl/MasterContext.java
@@ -170,7 +170,14 @@ public class MasterContext {
             return;
         }
 
-        DAG dag = deserializeDAG();
+        DAG dag;
+        try {
+            dag = deserializeDAG();
+        } catch (RuntimeException e) {
+            logger.warning("DAG deserialization failed", e);
+            finalizeJob(e);
+            return;
+        }
         // save a copy of the vertex list, because it is going to change
         vertices = new HashSet<>();
         dag.iterator().forEachRemaining(vertices::add);
@@ -197,8 +204,7 @@ public class MasterContext {
         MembersView membersView = getMembersView();
         ClassLoader previousCL = swapContextClassLoader(coordinationService.getClassLoader(jobId));
         try {
-            logger.info("Start executing " + jobIdString() + ", status " + jobStatus()
-                    + "\n" + dag);
+            logger.info("Start executing " + jobIdString() + ", status " + jobStatus() + "\n" + dag);
             logger.fine("Building execution plan for " + jobIdString());
             executionPlanMap = createExecutionPlans(nodeEngine, membersView, dag, getJobConfig(), lastSnapshotId);
         } catch (Exception e) {
@@ -541,25 +547,26 @@ public class MasterContext {
 
         completeVertices(failure);
 
-        long completionTime = System.currentTimeMillis();
         if (shouldRestart(failure)) {
             scheduleRestart();
             return;
         }
 
-        long elapsed = completionTime - jobStartTime;
+        long elapsed = System.currentTimeMillis() - jobStartTime;
         if (isSuccess(failure)) {
-            logger.info("Execution of " + jobIdString() + " completed in " + elapsed + " ms");
+            logger.info(String.format("Execution of %s completed in %,d ms", jobIdString(), elapsed));
         } else {
-            logger.warning("Execution of " + jobIdString()
-                    + " failed in " + elapsed + " ms", failure);
+            logger.warning(String.format("Execution of %s failed after %,d ms", jobIdString(), elapsed), failure);
         }
 
+        finalizeJob(failure);
+    }
+
+    private void finalizeJob(@Nullable Throwable failure) {
         try {
-            coordinationService.completeJob(this, executionId, completionTime, failure);
+            coordinationService.completeJob(this, executionId, System.currentTimeMillis(), failure);
         } catch (RuntimeException e) {
-            logger.warning("Completion of " + jobIdString()
-                    + " failed in " + elapsed + " ms", failure);
+            logger.warning("Completion of " + jobIdString() + " failed", failure);
         } finally {
             setFinalResult(failure);
         }
--- hazelcast-jet-core/src/test/java/com/hazelcast/jet/core/ExceptionHandlingTest.java
+++ hazelcast-jet-core/src/test/java/com/hazelcast/jet/core/ExceptionHandlingTest.java
@@ -21,11 +21,17 @@ import com.hazelcast.jet.core.TestProcessors.ProcessorThatFailsInComplete;
 import com.hazelcast.jet.core.TestProcessors.ProcessorThatFailsInInit;
 import com.hazelcast.jet.function.DistributedSupplier;
 import com.hazelcast.nio.Address;
+import com.hazelcast.nio.serialization.HazelcastSerializationException;
 import com.hazelcast.test.HazelcastParallelClassRunner;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+
+import javax.annotation.Nonnull;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
 
 import static com.hazelcast.jet.core.Edge.between;
 import static com.hazelcast.jet.core.TestUtil.executeAndPeel;
@@ -154,5 +160,66 @@ public class ExceptionHandlingTest extends JetTestSupport {
 
         // When
         executeAndPeel(instance.newJob(dag));
+    }
+
+    @Test
+    public void when_deserializationOnMembersFails_then_jobSubmissionFails() throws Throwable {
+        instance = createJetMember();
+
+        // Given
+        DAG dag = new DAG();
+        // this is designed to fail when member deserializes the execution plan while executing
+        // the InitOperation
+        dag.newVertex("faulty", (ProcessorMetaSupplier) addresses -> address -> new NotDeserializableProcessorSupplier());
+
+        // Then
+        expectedException.expect(HazelcastSerializationException.class);
+        expectedException.expectMessage("fake.Class");
+
+        // When
+        executeAndPeel(instance.newJob(dag));
+    }
+
+    @Test
+    public void when_deserializationOnMasterFails_then_jobSubmissionFails() throws Throwable {
+        instance = createJetMember();
+
+        // Given
+        DAG dag = new DAG();
+        // this is designed to fail when the master member deserializes the DAG
+        dag.newVertex("faulty", new NotDeserializableProcessorMetaSupplier());
+
+        // Then
+        expectedException.expect(HazelcastSerializationException.class);
+        expectedException.expectMessage("fake.Class");
+
+        // When
+        executeAndPeel(instance.newJob(dag));
+    }
+
+    private static class NotDeserializableProcessorSupplier implements ProcessorSupplier {
+        @Nonnull
+        @Override
+        public Collection<? extends Processor> get(int count) {
+            throw new UnsupportedOperationException("should not get here");
+        }
+
+        private void readObject(java.io.ObjectInputStream stream) throws ClassNotFoundException {
+            // simulate deserialization failure
+            throw new ClassNotFoundException("fake.Class");
+        }
+    }
+
+    private static class NotDeserializableProcessorMetaSupplier implements ProcessorMetaSupplier {
+        @Nonnull
+        @Override
+        public Function<Address, ProcessorSupplier> get(@Nonnull List<Address> addresses) {
+            throw new UnsupportedOperationException("should not get here");
+        }
+
+        private void readObject(java.io.ObjectInputStream stream) throws ClassNotFoundException {
+            // simulate deserialization failure
+            throw new ClassNotFoundException("fake.Class");
+        }
     }
 }
