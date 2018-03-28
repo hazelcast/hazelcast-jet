fbdc8f66d5fe93d00ce0b93c5057d6780b93fde3
Viliam Durina <viliam-durina@users.noreply.github.com>
2018-03-28 14:42:51 +0200

    Idle CPU usage improvements
    - Mark StreamEventJournalP cooperative
    - Higher idle time for blocking processors
    - Avoid overflow of idleCount
    
    Related to #775

--- hazelcast-jet-core/src/main/java/com/hazelcast/jet/impl/connector/StreamEventJournalP.java
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/impl/connector/StreamEventJournalP.java
@@ -246,7 +246,7 @@ public final class StreamEventJournalP<E, T> extends AbstractProcessor {
 
     @Override
     public boolean isCooperative() {
-        return false;
+        return true;
     }
 
     private void initialRead() {
