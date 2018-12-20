7ade67c9ab2058a964cd0481d7dc92e132ee3f70
Viliam Durina <viliam-durina@users.noreply.github.com>
2018-12-20 13:20:48 +0100

    Add test

--- hazelcast-jet-kafka/src/main/java/com/hazelcast/jet/kafka/impl/StreamKafkaP.java
+++ hazelcast-jet-kafka/src/main/java/com/hazelcast/jet/kafka/impl/StreamKafkaP.java
@@ -93,7 +93,7 @@ public final class StreamKafkaP<K, V, T> extends AbstractProcessor {
         this.properties = properties;
         this.topics = topics;
         this.projectionFn = projectionFn;
-        watermarkSourceUtil = WatermarkSourceUtil.create(eventTimePolicy);
+        watermarkSourceUtil = new WatermarkSourceUtil<>(eventTimePolicy);
         partitionCounts = new int[topics.size()];
     }
 
