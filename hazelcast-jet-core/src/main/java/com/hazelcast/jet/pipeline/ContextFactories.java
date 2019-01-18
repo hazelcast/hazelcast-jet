9de98f2ac9776e593c18bf8c5c374be7a929285a
Marko Topolnik <marko@hazelcast.com>
2019-01-18 11:15:22 +0100

    Javadoc wording

--- hazelcast-jet-core/src/main/java/com/hazelcast/jet/pipeline/ContextFactories.java
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/pipeline/ContextFactories.java
@@ -69,11 +69,11 @@ public final class ContextFactories {
      * the data in the map can change while the job is running so you can keep
      * the enriching dataset up-to-date.
      * <p>
-     * Instead of using this method, you can use {@link
+     * Instead of using this factory, you can call {@link
      * GeneralStage#mapUsingIMapAsync(IMap, DistributedBiFunction)} or {@link
      * GeneralStageWithKey#mapUsingIMapAsync(IMap, DistributedBiFunction)}.
      * <p>
-     * If you want to use a sync method on the map, call {@link
+     * If you plan to use a sync method on the map, call {@link
      * ContextFactory#nonCooperative()} on the returned factory.
      *
      * @param mapName name of the map used as context
