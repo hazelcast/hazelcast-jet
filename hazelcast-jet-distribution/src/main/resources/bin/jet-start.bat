0ada8483ed998a6238274c563cd931ce0b5a66f6
Can Gencer <cgencer@gmail.com>
2019-03-26 14:34:40 +0100

    more fixes to batch file

--- hazelcast-jet-distribution/src/main/resources/bin/jet-start.bat
+++ hazelcast-jet-distribution/src/main/resources/bin/jet-start.bat
@@ -4,7 +4,7 @@ SETLOCAL
 
 if NOT DEFINED JAVA_HOME goto error
 set RUN_JAVA=%JAVA_HOME%\bin\java
-set JET_HOME="%~dp0.."
+set JET_HOME=%~dp0..
 
 REM ******* you can enable following variables by uncommenting them
 
@@ -14,21 +14,20 @@ REM set MIN_HEAP_SIZE=1G
 REM ******* maximum heap size
 REM set MAX_HEAP_SIZE=1G
 
-
 if NOT "%MIN_HEAP_SIZE%" == "" (
-	set JAVA_OPTS=%JAVA_OPTS% -Xms%MIN_HEAP_SIZE%
+    set JAVA_OPTS=%JAVA_OPTS% -Xms%MIN_HEAP_SIZE%
 )
 
 if NOT "%MAX_HEAP_SIZE%" == "" (
-	set JAVA_OPTS=%JAVA_OPTS% -Xmx%MAX_HEAP_SIZE%
+    set JAVA_OPTS=%JAVA_OPTS% -Xmx%MAX_HEAP_SIZE%
 )
 
 set JAVA_OPTS=%JAVA_OPTS%^
- -Dhazelcast.jet.config=%JET_HOME%\config\hazelcast-jet.xml^
- -Dhazelcast.config=%JET_HOME%\config\hazelcast.xml^
- -Djet.home=%JET_HOME%
+ "-Dhazelcast.jet.config=%JET_HOME%\config\hazelcast-jet.xml"^
+ "-Dhazelcast.config=%JET_HOME%\config\hazelcast.xml"^
+ "-Djet.home=%JET_HOME%"
 
-set CLASSPATH=%JET_HOME%\lib\${hazelcast.jet.artifact}-${project.version}.jar;%CLASSPATH%
+set CLASSPATH="%JET_HOME%\lib\${hazelcast.jet.artifact}-${project.version}.jar";%CLASSPATH%
 
 ECHO ########################################
 ECHO # RUN_JAVA=%RUN_JAVA%
@@ -37,7 +36,7 @@ ECHO # CLASSPATH=%CLASSPATH%
 ECHO # starting now....
 ECHO ########################################
 
-start "hazelcast-jet" "%RUN_JAVA%" %JAVA_OPTS% -cp "%CLASSPATH%" "com.hazelcast.jet.server.StartServer"
+start "hazelcast-jet" "%RUN_JAVA%" %JAVA_OPTS% -cp %CLASSPATH% com.hazelcast.jet.server.StartServer"
 goto endofscript
 
 :error
