82a490ecbb044e1eb1ffc3fd8f6dd04560eb8e9a
Jaromir Hamala <jaromir.hamala@gmail.com>
2019-06-03 21:36:02 +0200

    style issues fixed
    thanks to Marko for his comments!

--- hazelcast-jet-core/src/main/java/com/hazelcast/jet/pipeline/DevSources.java
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/pipeline/DevSources.java
@@ -18,6 +18,7 @@ package com.hazelcast.jet.pipeline;
 
 import com.hazelcast.jet.function.SupplierEx;
 
+import javax.annotation.Nonnull;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
@@ -28,6 +29,7 @@ import java.util.concurrent.atomic.AtomicLong;
  * It's often convenient to have easy-to-use sources with well defined
  * behaviour when developing a streaming application.
  *
+ * @since 3.1
  */
 public final class DevSources {
     private static final AtomicLong COUNTER = new AtomicLong();
@@ -38,69 +40,85 @@ public final class DevSources {
     }
 
     /**
-     * Streaming source which emit with fixed rate. It attempts to compensate for various system hiccups so the rate
-     * over a period of time is constant.
+     * Streaming source which emits local wall-clock timestamps with specified
+     * minimum delay between items. It attempts to compensate for various
+     * system hiccups so the rate over a period of time is constant.
+     * <p>
+     * Each item is a local wall-clock timestamp. The source is not distributed
+     * it means only a single Jet instance will emit items.
      *
-     * Each item is a local wall-clock timestamp.
-     * The source is not distributed it means only a single Jet instance will emit items.
-     *
-     * @param period period which to emit
+     * @param period   period which to emit
      * @param timeUnit units for period
      * @return source emitting with a fixed rate
      */
-    public static StreamSource<Long> fixedRate(long period, TimeUnit timeUnit) {
+    @Nonnull
+    public static StreamSource<Long> fixedRate(long period, @Nonnull TimeUnit timeUnit) {
         return fixedRate(period, timeUnit, System::currentTimeMillis);
     }
 
     /**
-     * Streaming source which emits with specified minimum delay between items.
-     *
-     * It won't emit unless elapsed time since last emit is at least the specified delay. There is no upper bound
-     * on the delay at that's partially driven by the Jet engine. In practice it will behave similar to fixedDelay.
-     *
-     * The source is not distributed it means only a single Jet instance will emit items.
+     * Streaming source which emits local wall-clock timestamps with specified
+     * minimum delay between items.
+     * <p>
+     * It won't emit unless elapsed time since last emit is at least the
+     * specified delay. There is no upper bound on the delay at that's partially
+     * driven by the Jet engine. In practice it will behave similar to
+     * fixedDelay.
+     * <p>
+     * The source is not distributed it means only a single Jet instance
+     * will emit items.
      *
      * @param delay minimum delay between emitting
      * @param timeUnit units of the delay
      * @return source emitting with a specified minimum delay
      */
-    public static StreamSource<Long> minimumDelay(long delay, TimeUnit timeUnit) {
+    @Nonnull
+    public static StreamSource<? extends Long> minimumDelay(long delay, @Nonnull TimeUnit timeUnit) {
         return minimumDelay(delay, timeUnit, System::currentTimeMillis);
     }
 
     /**
-     * Streaming source which emit items at fixed rate. It attempts to compensate for various system hiccups so
-     * the rate over a period of time is constant.
-     *
+     * Streaming source which emit items at fixed rate. It attempts to compensate
+     * for various system hiccups so the rate over a period of time is constant.
+     * <p>
      * You have to provide your own item supplier.
-     * The source is not distributed it means only a single Jet instance will emit items.
-     *
+     * The source is not distributed it means only a single Jet instance will
+     * emit items.
+     * <p>
      * @param period period which to emit
      * @param timeUnit units for period
-     * @param itemSupplier supplier of items to be emitted. It's called whenever a new item is about to be emitted
+     * @param itemSupplier supplier of items to be emitted. It's called
+     *                     whenever a new item is about to be emitted
      * @param <T> type of emitted items
      * @return source emitting with a fixed rate
      */
-    public static <T> StreamSource<T> fixedRate(long period, TimeUnit timeUnit, SupplierEx<T> itemSupplier) {
+    @Nonnull
+    public static <T> StreamSource<T> fixedRate(
+            long period, @Nonnull TimeUnit timeUnit, @Nonnull SupplierEx<? extends T> itemSupplier) {
         return triggerDrivenStreamSource(() -> new FixedRateTrigger(period, timeUnit), itemSupplier);
     }
 
     /**
      * Streaming source which emits with specified minimum delay between items.
-     *
-     * It won't emit unless elapsed time since last emit is at least the specified delay. There is no upper bound
-     * on the delay at that's partially driven by the Jet engine. In practice it will behave similar to fixedDelay.
-     *
+     * <p>
+     * It won't emit unless elapsed time since last emit is at least the
+     * specified delay. There is no upper bound on the delay at that's
+     * partially driven by the Jet engine. In practice it will behave similar
+     * to fixedDelay.
+     * <p>
      * You have to provide your own item supplier.
      * The source is not distributed it means only a single Jet instance will emit items.
      *
      * @param delay delay minimum delay between emitting
      * @param timeUnit units of the delay
-     * @param itemSupplier supplier of items to be emitted. It's called whenever a new item is about to be emitted
+     * @param itemSupplier supplier of items to be emitted. It's called
+     *                     whenever a new item is about to be emitted
      * @param <T> type of emitted items
      * @return source emitting with a specified minimum delay
      */
-    public static <T> StreamSource<T> minimumDelay(long delay, TimeUnit timeUnit, SupplierEx<T> itemSupplier) {
+    @Nonnull
+    public static <T> StreamSource<T> minimumDelay(
+            long delay, @Nonnull TimeUnit timeUnit, @Nonnull SupplierEx<? extends T> itemSupplier) {
         return triggerDrivenStreamSource(() -> new MinDelayTrigger(delay, timeUnit), itemSupplier);
     }
 
@@ -111,12 +129,14 @@ public final class DevSources {
      * @param <T> type of the element
      * @return source emitting all specified items
      */
-    public static <T> BatchSource<T> of(T... items) {
+    @Nonnull
+    public static <T> BatchSource<T> of(@Nonnull T...items) {
         return fromIterator(() -> Arrays.asList(items).iterator());
     }
 
-    private static <T> StreamSource<T> triggerDrivenStreamSource(SupplierEx<Trigger> triggerSupplier,
-                                                                 SupplierEx<T> itemSupplier) {
+    @Nonnull
+    private static <T> StreamSource<T> triggerDrivenStreamSource(
+            @Nonnull SupplierEx<? extends Trigger> triggerSupplier, @Nonnull SupplierEx<? extends T> itemSupplier) {
         return SourceBuilder.stream(newName(), c -> triggerSupplier.get())
                 .<T>fillBufferFn((trigger, buffer) -> {
                     while (trigger.shouldEmit()) {
@@ -125,7 +145,8 @@ public final class DevSources {
                 }).build();
     }
 
-    private static <T> BatchSource<T> fromIterator(SupplierEx<Iterator<T>> iteratorSupplier) {
+    @Nonnull
+    private static <T> BatchSource<T> fromIterator(@Nonnull SupplierEx<Iterator<? extends T>> iteratorSupplier) {
         return SourceBuilder.batch(newName(), c -> iteratorSupplier.get())
                 .<T>fillBufferFn((i, b) -> {
                     if (i.hasNext()) {
@@ -137,6 +158,7 @@ public final class DevSources {
                 }).build();
     }
 
+    @Nonnull
     private static String newName() {
         return NAME_PREFIX + COUNTER.getAndIncrement();
     }
@@ -149,7 +171,7 @@ public final class DevSources {
         private long lastEmit;
         private final long delayNanos;
 
-        private MinDelayTrigger(long period, TimeUnit timeUnit) {
+        private MinDelayTrigger(long period, @Nonnull TimeUnit timeUnit) {
             this.lastEmit = System.nanoTime();
             this.delayNanos = timeUnit.toNanos(period);
         }
@@ -169,7 +191,7 @@ public final class DevSources {
         private long lastEmit;
         private final long periodNanos;
 
-        private FixedRateTrigger(long period, TimeUnit timeUnit) {
+        private FixedRateTrigger(long period, @Nonnull TimeUnit timeUnit) {
             this.lastEmit = System.nanoTime();
             this.periodNanos = timeUnit.toNanos(period);
         }
