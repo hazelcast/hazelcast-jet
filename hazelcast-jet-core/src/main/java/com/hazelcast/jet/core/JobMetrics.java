bd35ee645d663950c17486b4049fb5f56d4303dc
Bartok Jozsef <jozsi@hazelcast.com>
2019-07-02 16:00:53 +0300

    Add some tag based querying to JobMetrics

--- hazelcast-jet-core/src/main/java/com/hazelcast/jet/core/JobMetrics.java
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/core/JobMetrics.java
@@ -16,15 +16,20 @@
 
 package com.hazelcast.jet.core;
 
+import com.hazelcast.internal.metrics.MetricsUtil;
 import com.hazelcast.spi.annotation.Beta;
 
 import javax.annotation.Nonnull;
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Contains information related to job-specific metrics.
@@ -32,37 +37,28 @@ import java.util.Objects;
 @Beta
 public final class JobMetrics implements Serializable {
 
-    /**
-     * Static empty instance, contains no metrics
-     */
+    /** Static empty instance, contains no metrics */
     public static final JobMetrics EMPTY = new JobMetrics();
 
     private final Map<String, Long> metrics;
 
-    /**
-     * Builds an empty {@link JobMetrics} object.
-     */
+    /** Builds an empty {@link JobMetrics} object. */
     private JobMetrics() {
         this(Collections.emptyMap());
     }
 
-    /**
-     * Builds a {@link JobMetrics} object based on a key-value map of metrics data.
-     */
+    /** Builds a {@link JobMetrics} object based on a key-value map of metrics data. */
     private JobMetrics(Map<String, Long> metrics) {
+        Objects.requireNonNull(metrics);
         this.metrics = Collections.unmodifiableMap(metrics);
     }
 
-    /**
-     * Builds a {@link JobMetrics} object based on a key-value map of metrics data.
-     */
+    /** Builds a {@link JobMetrics} object based on a key-value map of metrics data. */
     public static JobMetrics of(Map<String, Long> metrics) {
         return new JobMetrics(new HashMap<>(metrics));
     }
 
-    /**
-     * Returns a collection containing the names of all job specific metrics available.
-     */
+    /** Returns a collection containing the names of all job specific metrics available. */
     @Nonnull
     Collection<String> getMetricNames() {
         return metrics.keySet();
@@ -77,20 +73,50 @@ public final class JobMetrics implements Serializable {
         return metrics.get(name);
     }
 
-    @Override
-    public String toString() {
-        return JobMetrics.class.getSimpleName() + "{" + metrics + "}";
-    }
-
-    /**
-     * Merges two immutable {@link JobMetrics} instances into a third
-     * @param other
-     * @return
-     */
+    /** Merges two immutable {@link JobMetrics} instances into a third */
     public JobMetrics merge(JobMetrics other) {
         Map<String, Long> map = new HashMap<>();
         map.putAll(this.metrics);
         map.putAll(other.metrics);
         return new JobMetrics(map);
+    }
+
+    /** Returns a subset of the current metrics, which have the specified value for the specified tag. */
+    public JobMetrics withTag(String tag, String value) {
+        String nameToken = tag + '=' + MetricsUtil.escapeMetricNamePart(value);
+        Predicate<Map.Entry<String, Long>> filterPredicate = e ->
+                new HashSet<>(Arrays.asList(e.getKey().split("[,\\]\\[]"))).contains(nameToken);
+        Map<String, Long> filteredMetrics = metrics.entrySet().stream()
+                .filter(filterPredicate)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        return new JobMetrics(filteredMetrics);
+    }
+
+    /** Returns the number of metrics present.*/
+    public int size() {
+        return metrics.size();
+    }
+
+    @Override
+    public int hashCode() {
+        return metrics.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        if (obj == this) {
+            return true;
+        }
+
+        return Objects.equals(metrics, ((JobMetrics) obj).metrics);
+    }
+
+    @Override
+    public String toString() {
+        return JobMetrics.class.getSimpleName() + "{" + metrics + "}";
     }
 }
