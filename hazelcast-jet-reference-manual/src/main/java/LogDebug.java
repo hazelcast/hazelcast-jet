2f3749f41121b36017a6d2694a3203be3085ec7a
Bartok Jozsef <jozsi@hazelcast.com>
2019-10-03 13:42:07 +0300

    Describe user-metrics in refman

--- hazelcast-jet-reference-manual/src/main/java/LogDebug.java
+++ hazelcast-jet-reference-manual/src/main/java/LogDebug.java
@@ -16,10 +16,20 @@
 
 import com.hazelcast.jet.Jet;
 import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.Job;
 import com.hazelcast.jet.config.JetConfig;
+import com.hazelcast.jet.core.metrics.Counter;
+import com.hazelcast.jet.core.metrics.MetricsContext;
+import com.hazelcast.jet.core.metrics.ProvidesMetrics;
+import com.hazelcast.jet.function.PredicateEx;
+import com.hazelcast.jet.pipeline.BatchSource;
 import com.hazelcast.jet.pipeline.Pipeline;
+import com.hazelcast.jet.pipeline.Sink;
 import com.hazelcast.jet.pipeline.Sinks;
 import com.hazelcast.jet.pipeline.Sources;
+import com.hazelcast.jet.pipeline.test.TestSources;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 import static com.hazelcast.jet.Traversers.traverseArray;
 import static com.hazelcast.jet.aggregate.AggregateOperations.counting;
@@ -37,7 +47,7 @@ public class LogDebug {
         //tag::s2[]
         JetConfig config = new JetConfig();
         config.getHazelcastConfig()
-              .setProperty("hazelcast.logging.type", "log4j");
+                .setProperty("hazelcast.logging.type", "log4j");
         //end::s2[]
     }
 
@@ -45,19 +55,19 @@ public class LogDebug {
         //tag::s3[]
         Pipeline p = Pipeline.create();
         p.drawFrom(Sources.<String>list("inputList"))
-         .flatMap(line -> traverseArray(line.toLowerCase().split("\\W+")))
-         .filter(word -> !word.isEmpty())
-         .peek() // <1>
-         .groupingKey(wholeItem())
-         .aggregate(counting())
-         .drainTo(Sinks.map("counts"));
+                .flatMap(line -> traverseArray(line.toLowerCase().split("\\W+")))
+                .filter(word -> !word.isEmpty())
+                .peek() // <1>
+                .groupingKey(wholeItem())
+                .aggregate(counting())
+                .drainTo(Sinks.map("counts"));
         //end::s3[]
 
         //tag::s4[]
         JetInstance jet = Jet.newJetInstance();
         try {
             jet.getList("inputList")
-               .addAll(asList("The quick brown fox", "jumped over the lazy dog"));
+                    .addAll(asList("The quick brown fox", "jumped over the lazy dog"));
             jet.newJob(p).join();
         } finally {
             Jet.shutdownAll();
@@ -66,8 +76,43 @@ public class LogDebug {
     }
 
     static void s5() {
+        BatchSource<Long> source = TestSources.items(0L, 1L, 2L, 3L, 4L);
+        Sink<Long> sink = Sinks.logger();
+        Pipeline pipeline = Pipeline.create();
+
         //tag::s5[]
+        class Filter implements PredicateEx<Long>, ProvidesMetrics {
+
+            private Counter dropped;
+            private AtomicLong total;
+
+            @Override
+            public boolean testEx(Long l) throws Exception {
+                boolean even = l % 2L == 0;
+                if (!even) {
+                    dropped.increment();
+                }
+                total.incrementAndGet();
+                return even;
+            }
+
+            @Override
+            public void registerMetrics(MetricsContext context) {
+                dropped = context.registerCounter("dropped");
+
+                total = new AtomicLong();
+                context.registerGauge("total", total::get);
+            }
+        }
+
+        pipeline.drawFrom(source)
+                .filter(new Filter())
+                .drainTo(sink);
         //end::s5[]
+
+        JetInstance jet = Jet.newJetInstance();
+        Job job = jet.newJob(pipeline);
+        job.join();
     }
 
     static void s6() {
