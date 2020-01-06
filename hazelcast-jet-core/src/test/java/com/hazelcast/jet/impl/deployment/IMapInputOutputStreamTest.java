f04b49e5d90fc856f54e35ce2b67a829b2bd5ee3
Emin Demirci <emin@hazelcast.com>
2020-01-06 12:56:42 +0300

    Increase test coverage

--- hazelcast-jet-core/src/test/java/com/hazelcast/jet/impl/deployment/IMapInputOutputStreamTest.java
+++ hazelcast-jet-core/src/test/java/com/hazelcast/jet/impl/deployment/IMapInputOutputStreamTest.java
@@ -22,12 +22,15 @@ import com.hazelcast.map.IMap;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -40,9 +43,66 @@ import static org.junit.Assert.assertTrue;
 @RunWith(HazelcastSerialClassRunner.class)
 public class IMapInputOutputStreamTest extends SimpleTestInClusterSupport {
 
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @BeforeClass
     public static void beforeClass() {
         initialize(1, null);
+    }
+
+    @Test
+    public void test_writeToClosedStream_then_throwsException() throws IOException {
+        expectedException.expect(IOException.class);
+        IMap<String, byte[]> map = instance().getMap(randomMapName());
+        IMapOutputStream outputStream = new IMapOutputStream(map, "test");
+
+        outputStream.close();
+        outputStream.write(5);
+    }
+
+    @Test
+    public void test_multipleCallsToCloseStream_then_flushesOnlyOnce() throws IOException {
+        IMap<String, byte[]> map = instance().getMap(randomMapName());
+        IMapOutputStream outputStream = new IMapOutputStream(map, "test");
+
+        outputStream.close();
+        long putOperationCountBeforeCloses = map.getLocalMapStats().getPutOperationCount();
+        outputStream.close();
+        outputStream.close();
+        outputStream.close();
+
+        assertEquals(putOperationCountBeforeCloses, map.getLocalMapStats().getPutOperationCount());
+    }
+
+    @Test
+    public void test_readFromClosedStream_then_throwsException() throws IOException {
+        expectedException.expect(IOException.class);
+        IMap<String, byte[]> map = instance().getMap(randomMapName());
+        map.put("test", new byte[] {0, 0, 0, 4});
+        IMapInputStream inputStream = new IMapInputStream(map, "test");
+
+        inputStream.close();
+        inputStream.read(new byte[] {1}, 0, 1);
+    }
+
+    @Test
+    public void test_writeOutOfBounds_then_throwsException() throws IOException {
+        expectedException.expect(IndexOutOfBoundsException.class);
+        IMap<String, byte[]> map = instance().getMap(randomMapName());
+        IMapOutputStream outputStream = new IMapOutputStream(map, "test");
+
+        outputStream.write(new byte[] {1}, 5, 5);
+    }
+
+    @Test
+    public void test_readOutOfBounds_then_throwsException() throws IOException {
+        expectedException.expect(IndexOutOfBoundsException.class);
+        IMap<String, byte[]> map = instance().getMap(randomMapName());
+        map.put("test", new byte[] {0, 0, 0, 4});
+        IMapInputStream inputStream = new IMapInputStream(map, "test");
+
+        inputStream.read(new byte[] {1}, 5, 5);
     }
 
     @Test
