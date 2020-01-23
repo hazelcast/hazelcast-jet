0005e02b55d9482ecc40c587a475bec3f3cd284f
Emin Demirci <emin@hazelcast.com>
2020-01-23 13:45:13 +0300

    Move to multiple license model

---
+++ plugins/hazelcast-jet-spring/src/test/java/com/hazelcast/jet/spring/SillyCalculator.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 Hazelcast Inc.
+ *
+ * Licensed under the Hazelcast Community License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://hazelcast.com/hazelcast-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.jet.spring;
+
+public class SillyCalculator implements Calculator {
+
+    @Override
+    public long multiply(long input) {
+        return -input;
+    }
+
+    @Override
+    public boolean filter(long input) {
+        return input % 2 == 0;
+    }
+}
