904fe0bf98a701f730a74c811fe44770d3aefd7d
Emin Demirci <emin@hazelcast.com>
2020-01-29 21:10:53 +0300

    Revert spring module

--- hazelcast-jet-spring/src/test/java/com/hazelcast/jet/spring/SourceSinkBuilderTest.java
+++ hazelcast-jet-spring/src/test/java/com/hazelcast/jet/spring/SourceSinkBuilderTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.hazelcast.jet.spring;
 
 import com.hazelcast.collection.IQueue;
