ef20265ecf3949640d0eacb6ffec4daca3a162a2
Marko Topolnik <marko@hazelcast.com>
2020-02-05 14:35:17 +0100

    Freeze gRPC versions installed via pip

--- extensions/python/src/main/resources/jet_to_python_init.sh
+++ extensions/python/src/main/resources/jet_to_python_init.sh
@@ -5,7 +5,7 @@ set -e -x
 
 python3 -m venv jet_to_python_env --system-site-packages
 source jet_to_python_env/bin/activate
-python3 -m pip install protobuf grpcio
+python3 -m pip install protobuf==3.11.3 grpcio==1.26.0
 [[ -f requirements.txt ]] && python3 -m pip install -r requirements.txt
 source ./jet_to_python_params.sh
 [[ -f init.sh ]] && ./init.sh
