3e6413cb47616c97bf62f9c32a70001eb1731463
Grzegorz Gierlach <gierlachg@gmail.com>
2020-02-06 16:38:40 +0100

    Fix spotbugs error.

--- hazelcast-jet-core/src/main/java/com/hazelcast/jet/config/JobConfig.java
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/config/JobConfig.java
@@ -38,7 +38,6 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.LinkedHashMap;
