9a8fcc2a2dde039ca3ace5e62fc635ebad916c18
Viliam Durina <viliam-durina@users.noreply.github.com>
2020-02-07 10:11:44 +0100

    Fix compilation

--- reference-manual/src/main/java/StockExchangeCoreApi.java
+++ reference-manual/src/main/java/StockExchangeCoreApi.java
@@ -95,7 +95,7 @@ public class StockExchangeCoreApi {
                         kwr.getKey(), kwr.getValue())
         ));
         Vertex sink = dag.newVertex("sink", SinkProcessors.writeFileP(
-                OUTPUT_DIR_NAME, UTF_8, null, null, true, Object::toString));
+                OUTPUT_DIR_NAME, UTF_8, null, null, true, Object::toString, false));
 
         tradeSource.localParallelism(1);
 
