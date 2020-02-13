a0744d312fd1729dc5c615a7f0e84a741941cccd
Can Gencer <cgencer@gmail.com>
2020-02-13 09:46:54 +0100

    Add trackingId

--- site/website/siteConfig.js
+++ site/website/siteConfig.js
@@ -106,6 +106,7 @@ const siteConfig = {
     // You may provide arbitrary config keys to be used as needed by your
     // template. For example, if you need your repo's URL...
     repoUrl: 'https://github.com/hazelcast/hazelcast-jet',
+    gaTrackingId: 'UA-158279495-1'
 };
 
 module.exports = siteConfig;
