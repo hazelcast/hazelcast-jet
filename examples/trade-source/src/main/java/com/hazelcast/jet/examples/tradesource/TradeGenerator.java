5f519ffce0d974ade2aa57510923a4e63b4d06ff
Bartok Jozsef <jozsi@hazelcast.com>
2020-03-09 11:03:46 +0200

    Add javadoc to trade source, do minor fixes

--- examples/trade-source/src/main/java/com/hazelcast/jet/examples/tradesource/TradeGenerator.java
+++ examples/trade-source/src/main/java/com/hazelcast/jet/examples/tradesource/TradeGenerator.java
@@ -37,7 +37,7 @@ import static java.util.stream.Collectors.toMap;
 
 public final class TradeGenerator {
     private static final int LOT = 100;
-    private static final int PRICE_UNITS_PER_CENT = 100;
+    private static final long PRICE_UNITS_PER_CENT = 1000L;
     private static final int NO_TIMEOUT = 0;
 
     private final List<String> tickers;
@@ -54,7 +54,7 @@ public final class TradeGenerator {
         this.tickers = loadTickers(numTickers);
         this.maxLagNanos = MILLISECONDS.toNanos(maxLagMillis);
         this.pricesAndTrends = tickers.stream()
-                .collect(toMap(t -> t, t -> new LongLongAccumulator(500, 10)));
+                .collect(toMap(t -> t, t -> new LongLongAccumulator(500, 5)));
         this.emitPeriodNanos = SECONDS.toNanos(1) / tradesPerSec;
         this.startTimeNanos = this.scheduledTimeNanos = System.nanoTime();
         this.endTimeNanos = timeoutSeconds <= 0 ? Long.MAX_VALUE :
@@ -62,14 +62,81 @@ public final class TradeGenerator {
         this.startTimeMillis = System.currentTimeMillis();
     }
 
+    /**
+     * Returns a source of an unbounded stream of trade events. The
+     * quantity of the returned trades will be a random multiple of an
+     * internal lot size ({@value #LOT}). The price will also be
+     * randomized, but not as an absolute value, but as a value with
+     * randomly varying change trends up or down. Both the price and the
+     * quantity are always positive.
+     * <p>
+     * The trades will be generated at the specified rate per second and
+     * their tickers will be from a limited set of names.
+     * <p>
+     * The event time of the generated trades will be evenly spaced
+     * across physical time, as dictated by the trades-per-second
+     * generation rate (ie. no disorder will be introduced by the source).
+     *
+     * @param tradesPerSec number of trades generated per second
+     *                     (globally, for all tickers)
+     * @return streaming source of trades
+     */
     public static StreamSource<Trade> tradeSource(int tradesPerSec) {
         return tradeSource(Integer.MAX_VALUE, tradesPerSec, 0);
     }
 
+    /**
+     * Returns a source of an unbounded stream of trade events. The
+     * quantity of the returned trades will be a random multiple of an
+     * internal lot size ({@value #LOT}). The price will also be
+     * randomized, but not as an absolute value, but as a value with
+     * randomly varying change trends up or down. Both the price and the
+     * quantity are always positive.
+     * <p>
+     * The trades will be generated at the specified rate per second and
+     * their tickers will be from a limited set of names, no more than
+     * the specified limit.
+     * <p>
+     * The event time of the generated trades will be evenly spaced
+     * across physical time, as dictated by the trades-per-second
+     * generation rate (ie. no disorder will be introduced by the source).
+     *
+     * @param numTickers   maximum number of ticker names for which
+     *                     trades will be generated
+     * @param tradesPerSec number of trades generated per second
+     *                     (globally, for all tickers)
+     * @return streaming source of trades
+     */
     public static StreamSource<Trade> tradeSource(int numTickers, int tradesPerSec) {
         return tradeSource(numTickers, tradesPerSec, 0);
     }
 
+    /**
+     * Returns a source of an unbounded stream of trade events. The
+     * quantity of the returned trades will be a random multiple of an
+     * internal lot size ({@value #LOT}). The price will also be
+     * randomized, but not as an absolute value, but as a value with
+     * randomly varying change trends up or down. Both the price and the
+     * quantity are always positive.
+     * <p>
+     * The trades will be generated at the specified rate per second and
+     * their tickers will be from a limited set of names, no more than
+     * the specified limit.
+     * <p>
+     * The event time of the generated trades would normally be evenly
+     * spaced across physical time, as dictated by the trades-per-second
+     * generation rate. However the trade source will introduce
+     * randomness in these timestamps, but no more than the specified
+     * upper limit.
+     *
+     * @param numTickers   maximum number of ticker names for which
+     *                     trades will be generated
+     * @param tradesPerSec number of trades generated per second
+     *                     (globally, for all tickers)
+     * @param maxLag       maximum random variance in the event time of
+     *                     the trades
+     * @return streaming source of trades
+     */
     public static StreamSource<Trade> tradeSource(int numTickers, int tradesPerSec, int maxLag) {
         return SourceBuilder
                 .timestampedStream("trade-source",
@@ -78,28 +145,20 @@ public final class TradeGenerator {
                 .build();
     }
 
-    private void generateTrades(TimestampedSourceBuffer<Trade> buf) {
-        if (scheduledTimeNanos >= endTimeNanos) {
-            return;
+    private static long getNextPrice(LongLongAccumulator priceAndDelta, ThreadLocalRandom rnd) {
+        long price = priceAndDelta.get1();
+        long delta = priceAndDelta.get2();
+        if (price + delta <= 0) {
+            //having a negative price doesn't make sense for most financial instruments
+            delta = -delta;
         }
-        ThreadLocalRandom rnd = ThreadLocalRandom.current();
-        long nowNanos = System.nanoTime();
-        while (scheduledTimeNanos <= nowNanos) {
-            String ticker = tickers.get(rnd.nextInt(tickers.size()));
-            LongLongAccumulator priceAndDelta = pricesAndTrends.get(ticker);
-            int price = (int) (priceAndDelta.get1() / PRICE_UNITS_PER_CENT);
-            priceAndDelta.add1(priceAndDelta.get2());
-            priceAndDelta.add2(rnd.nextLong(101) - 50);
-            long tradeTimeNanos = scheduledTimeNanos - (maxLagNanos > 0 ? rnd.nextLong(maxLagNanos) : 0L);
-            long tradeTimeMillis = startTimeMillis + NANOSECONDS.toMillis(tradeTimeNanos - startTimeNanos);
-            Trade trade = new Trade(tradeTimeMillis, ticker, rnd.nextInt(10) * LOT, price);
-            buf.add(trade, tradeTimeMillis);
-            scheduledTimeNanos += emitPeriodNanos;
-            if (scheduledTimeNanos > nowNanos) {
-                // Refresh current time before checking against scheduled time
-                nowNanos = System.nanoTime();
-            }
-        }
+        price = price + delta;
+        delta = delta + rnd.nextLong(101) - 50;
+
+        priceAndDelta.set1(price);
+        priceAndDelta.set2(delta);
+
+        return price;
     }
 
     private static List<String> loadTickers(long numTickers) {
@@ -112,6 +171,28 @@ public final class TradeGenerator {
                     .collect(toList());
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void generateTrades(TimestampedSourceBuffer<Trade> buf) {
+        if (scheduledTimeNanos >= endTimeNanos) {
+            return;
+        }
+        ThreadLocalRandom rnd = ThreadLocalRandom.current();
+        long nowNanos = System.nanoTime();
+        while (scheduledTimeNanos <= nowNanos) {
+            String ticker = tickers.get(rnd.nextInt(tickers.size()));
+            LongLongAccumulator priceAndDelta = pricesAndTrends.get(ticker);
+            long price = getNextPrice(priceAndDelta, rnd) / PRICE_UNITS_PER_CENT;
+            long tradeTimeNanos = scheduledTimeNanos - (maxLagNanos > 0 ? rnd.nextLong(maxLagNanos) : 0L);
+            long tradeTimeMillis = startTimeMillis + NANOSECONDS.toMillis(tradeTimeNanos - startTimeNanos);
+            Trade trade = new Trade(tradeTimeMillis, ticker, rnd.nextInt(1, 10) * LOT, price);
+            buf.add(trade, tradeTimeMillis);
+            scheduledTimeNanos += emitPeriodNanos;
+            if (scheduledTimeNanos > nowNanos) {
+                // Refresh current time before checking against scheduled time
+                nowNanos = System.nanoTime();
+            }
         }
     }
 }
