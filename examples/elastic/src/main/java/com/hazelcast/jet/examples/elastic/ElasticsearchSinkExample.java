fcf07faa3b97000f45d57a124ade9e03c5e69a0b
Frantisek Hartman <frant.hartm@gmail.com>
2020-03-30 15:45:37 +0200

    Examples for Elasticsearch connector

---
+++ examples/elastic/src/main/java/com/hazelcast/jet/examples/elastic/ElasticsearchSinkExample.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2008-2020, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.jet.examples.elastic;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.core.processor.SourceProcessors;
+import com.hazelcast.jet.elasticsearch.ElasticsearchSinks;
+import com.hazelcast.jet.pipeline.BatchSource;
+import com.hazelcast.jet.pipeline.Pipeline;
+import org.elasticsearch.action.index.IndexRequest;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static com.hazelcast.jet.pipeline.Pipeline.create;
+import static com.hazelcast.jet.pipeline.Sources.batchFromProcessor;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class ElasticsearchSinkExample {
+
+    public static void main(String[] args) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Pipeline p = create();
+            p.readFrom(files("src/main/resources/documents"))
+             .map(json -> mapper.readValue(json, new TypeReference<Map<String, Object>>() {
+             }))
+             .writeTo(ElasticsearchSinks.elasticsearch(map ->
+                     new IndexRequest("my-index").source(map)
+             ));
+
+            JetInstance jet = Jet.newJetInstance();
+            jet.newJob(p).join();
+        } catch (Exception e) {
+            Jet.shutdownAll();
+        }
+    }
+
+    public static BatchSource<String> files(String directory) {
+        return batchFromProcessor("filesSource(" + new File(directory) + ')',
+                SourceProcessors.readFilesP(directory, "*", false,
+                        path -> Stream.of(new String(Files.readAllBytes(path), UTF_8))));
+    }
+
+}
