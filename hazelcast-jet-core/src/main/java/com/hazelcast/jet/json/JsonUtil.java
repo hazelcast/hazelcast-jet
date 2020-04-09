277c229841758f7527dd1dadff750c986b056d82
Ali Gurbuz <ali@hazelcast.com>
2020-04-09 10:53:19 +0300

    add StreamJsonParser and JsonUtil

---
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/json/JsonUtil.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2008-2020, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.jet.json;
+
+import com.hazelcast.core.HazelcastJsonValue;
+import com.hazelcast.function.FunctionEx;
+import com.hazelcast.internal.json.Json;
+import com.hazelcast.internal.json.JsonObject;
+import com.hazelcast.internal.json.JsonValue;
+import com.hazelcast.jet.pipeline.BatchStage;
+
+import java.util.Map.Entry;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static com.hazelcast.jet.Util.entry;
+
+/**
+ * todo add proper javadoc
+ */
+public final class JsonUtil {
+
+    private JsonUtil() {
+    }
+
+    /* map to HazelcastJsonValue */
+
+    public static <T> FunctionEx<? super T, Entry<HazelcastJsonValue, HazelcastJsonValue>>
+    mapHazelcastJsonValue(
+            FunctionEx<? super T, String> toKeyFn,
+            FunctionEx<? super T, String> toValueFn
+    ) {
+        return object -> entry(hazelcastJsonValue(toKeyFn.apply(object)), hazelcastJsonValue(toValueFn.apply(object)));
+    }
+
+    public static <K, V> FunctionEx<? super Entry<? super K, ? super V>,
+            Entry<HazelcastJsonValue, HazelcastJsonValue>>
+    mapHazelcastJsonValue() {
+        return mapHazelcastJsonValue(e -> e.getKey().toString(), e -> e.getValue().toString());
+    }
+
+    /* transform to HazelcastJsonValue */
+
+    public static <T> FunctionEx<BatchStage<? extends T>,
+            BatchStage<Entry<HazelcastJsonValue, HazelcastJsonValue>>>
+    transformHazelcastJsonValue(
+            FunctionEx<? super T, String> toKeyFn,
+            FunctionEx<? super T, String> toValueFn
+    ) {
+        return upstream -> upstream.map(mapHazelcastJsonValue(toKeyFn, toValueFn));
+    }
+
+//    public static <K, V> FunctionEx<BatchStage<? extends Entry<? super K, ? super V>>,
+//            BatchStage<Entry<HazelcastJsonValue, HazelcastJsonValue>>>
+//    transformHazelcastJsonValue() {
+//        return upstream -> upstream.map(mapHazelcastJsonValue());
+//    }
+
+    public static <T> HazelcastJsonValue hazelcastJsonValue(T object) {
+        return new HazelcastJsonValue(object.toString());
+    }
+
+
+    /* Map and FlatMap Functions*/
+
+    public static <T> FunctionEx<? super T, JsonValue> jsonValueMapFn() {
+        return object -> Json.parse(object.toString());
+    }
+
+    public static <T> FunctionEx<? super T, JsonObject> jsonObjectMapFn() {
+        return object -> Json.parse(object.toString()).asObject();
+    }
+
+    public static <T> FunctionEx<? super T, Stream<JsonValue>> jsonValueFlatMapFn() {
+        return object -> {
+            JsonValue jsonValue = Json.parse(object.toString());
+            if (jsonValue.isArray()) {
+                return StreamSupport.stream(jsonValue.asArray().spliterator(), false);
+            }
+            return Stream.of(jsonValue);
+        };
+    }
+
+    public static <T> FunctionEx<? super T, Stream<JsonObject>> jsonObjectFlatMapFn() {
+        return object -> {
+            JsonValue jsonValue = Json.parse(object.toString());
+            if (jsonValue.isArray()) {
+                return StreamSupport.stream(jsonValue.asArray().spliterator(), false)
+                                    .map(JsonValue::asObject);
+            }
+            return Stream.of(jsonValue.asObject());
+        };
+    }
+
+}
--- hazelcast-jet-core/src/test/java/com/hazelcast/jet/impl/connector/ReadFilesPTest.java
+++ hazelcast-jet-core/src/test/java/com/hazelcast/jet/impl/connector/ReadFilesPTest.java
@@ -17,6 +17,7 @@
 package com.hazelcast.jet.impl.connector;
 
 import com.hazelcast.collection.IList;
+import com.hazelcast.internal.json.JsonObject;
 import com.hazelcast.jet.SimpleTestInClusterSupport;
 import com.hazelcast.jet.Util;
 import com.hazelcast.jet.pipeline.Pipeline;
@@ -27,6 +28,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map.Entry;
@@ -40,6 +42,7 @@ public class ReadFilesPTest extends SimpleTestInClusterSupport {
 
     private File directory;
     private IList<Entry<String, String>> list;
+    private IList<JsonObject> listJson;
 
     @BeforeClass
     public static void beforeClass() {
@@ -50,11 +53,12 @@ public class ReadFilesPTest extends SimpleTestInClusterSupport {
     public void setup() throws Exception {
         directory = createTempDirectory();
         list = instance().getList("writer");
+        listJson = instance().getList("writerJson");
     }
 
     @Test
     public void test_smallFiles() throws Exception {
-        Pipeline p = buildDag(null);
+        Pipeline p = pipeline(null);
 
         File file1 = new File(directory, randomName());
         appendToFile(file1, "hello", "world");
@@ -70,7 +74,7 @@ public class ReadFilesPTest extends SimpleTestInClusterSupport {
 
     @Test
     public void test_largeFile() throws Exception {
-        Pipeline p = buildDag(null);
+        Pipeline p = pipeline(null);
 
         File file1 = new File(directory, randomName());
         final int listLength = 10000;
@@ -79,11 +83,13 @@ public class ReadFilesPTest extends SimpleTestInClusterSupport {
         instance().newJob(p).join();
 
         assertEquals(listLength, list.size());
+
+        finishDirectory(file1);
     }
 
     @Test
     public void when_glob_the_useGlob() throws Exception {
-        Pipeline p = buildDag("file2.*");
+        Pipeline p = pipeline("file2.*");
 
         File file1 = new File(directory, "file1.txt");
         appendToFile(file1, "hello", "world");
@@ -99,7 +105,7 @@ public class ReadFilesPTest extends SimpleTestInClusterSupport {
 
     @Test
     public void when_directory_then_ignore() {
-        Pipeline p = buildDag(null);
+        Pipeline p = pipeline(null);
 
         File file1 = new File(directory, randomName());
         assertTrue(file1.mkdir());
@@ -111,7 +117,43 @@ public class ReadFilesPTest extends SimpleTestInClusterSupport {
         finishDirectory(file1);
     }
 
-    private Pipeline buildDag(String glob) {
+    @Test
+    public void testJsonFiles_when_rootElementObject_thenSingleJsonObject() throws IOException {
+        Pipeline p = pipelineJson();
+
+        File file1 = new File(directory, randomName());
+        appendToFile(file1, "{\"string\": \"hello world\", \"int\": 5, \"bool\": true}");
+        File file2 = new File(directory, randomName());
+        appendToFile(file2, "{\"string\": \"hello jupiter\", \"int\": 8, \"bool\": false}");
+
+        instance().newJob(p).join();
+
+        assertEquals(2, listJson.size());
+        JsonObject object = listJson.get(0);
+
+        assertTrue(object.getString("string", null).startsWith("hello"));
+        finishDirectory(file1, file2);
+    }
+
+    @Test
+    public void testJsonFiles_when_rootElementArray_thenJsonObjects() throws IOException {
+        Pipeline p = pipelineJson();
+
+        File file1 = new File(directory, randomName());
+        appendToFile(file1, "[{\"string\": \"hello world\", \"int\": 5}, {\"string\": \"hello venus\"}]");
+        File file2 = new File(directory, randomName());
+        appendToFile(file2, "[{\"string\": \"hello jupiter\", \"int\": 8}, {\"string\": \"hello mars\"}]");
+
+        instance().newJob(p).join();
+
+        assertEquals(4, listJson.size());
+        JsonObject object = listJson.get(0);
+
+        assertTrue(object.getString("string", null).startsWith("hello"));
+        finishDirectory(file1, file2);
+    }
+
+    private Pipeline pipeline(String glob) {
         Pipeline p = Pipeline.create();
         p.readFrom(Sources.filesBuilder(directory.getPath())
                           .glob(glob == null ? "*" : glob)
@@ -121,7 +163,16 @@ public class ReadFilesPTest extends SimpleTestInClusterSupport {
         return p;
     }
 
-    private void finishDirectory(File ... files) {
+    private Pipeline pipelineJson() {
+        Pipeline p = Pipeline.create();
+        p.readFrom(Sources.filesBuilder(directory.getPath())
+                          .buildJson())
+         .writeTo(Sinks.list(listJson));
+
+        return p;
+    }
+
+    private void finishDirectory(File... files) {
         for (File file : files) {
             assertTrue(file.delete());
         }
