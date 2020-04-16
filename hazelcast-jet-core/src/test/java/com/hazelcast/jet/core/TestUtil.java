8d5b71feb26c5c5bd6794a3731ab7aee976d90c3
Frantisek Hartman <frant.hartm@gmail.com>
2020-04-16 14:33:55 +0200

    Fix flaky test where job can be completed from JobResult
    Reproducible by adding delay to doInvokeJoinJob in AbstractJobProxy.
    
    Fixes# 2072

--- hazelcast-jet-core/src/test/java/com/hazelcast/jet/core/TestUtil.java
+++ hazelcast-jet-core/src/test/java/com/hazelcast/jet/core/TestUtil.java
@@ -69,7 +69,9 @@ public final class TestUtil {
         boolean found = false;
         Throwable t = caught;
         while (!found && t != null) {
-            found = Objects.equals(t.getMessage(), expected.getMessage()) && t.getClass() == expected.getClass();
+            found = Objects.equals(t.getMessage(), expected.getMessage()) && t.getClass() == expected.getClass() ||
+                    (t.getMessage().contains(expected.getMessage()) &&
+                            t.getMessage().contains(expected.getClass().getName()));
             t = t.getCause();
         }
 
