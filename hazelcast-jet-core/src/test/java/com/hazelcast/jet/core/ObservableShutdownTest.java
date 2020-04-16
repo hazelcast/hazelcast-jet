7fd53557685e205b9cab963f22df78aa94bb05fc
Bartok Jozsef <jozsi@hazelcast.com>
2020-04-16 10:59:04 +0300

    Make checkstyle happy

--- hazelcast-jet-core/src/test/java/com/hazelcast/jet/core/ObservableShutdownTest.java
+++ hazelcast-jet-core/src/test/java/com/hazelcast/jet/core/ObservableShutdownTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2008-2020, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hazelcast.jet.core;
 
 import com.hazelcast.jet.JetInstance;
@@ -32,10 +48,15 @@ public class ObservableShutdownTest {
     private static final int MEMBER_COUNT = 3;
 
     private JetTestInstanceFactory factory;
+
     private JetInstance[] members;
     private JetInstance client;
-    private Observable<Long> memberObservable, clientObservable;
-    private TestObserver memberObserver, clientObserver;
+
+    private Observable<Long> memberObservable;
+    private Observable<Long> clientObservable;
+
+    private TestObserver memberObserver;
+    private TestObserver clientObserver;
 
     @Before
     public void before() {
@@ -107,7 +128,7 @@ public class ObservableShutdownTest {
 
         @Override
         public void onError(@Nonnull Throwable throwable) {
-            fail("Errors aren't expected: " +throwable.getMessage());
+            fail("Errors aren't expected: " + throwable.getMessage());
         }
 
         @Override
