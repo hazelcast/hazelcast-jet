630c148b818a6aff4ee0ba72173b39982702e66f
Bartok Jozsef <jozsi@hazelcast.com>
2020-04-30 12:05:20 +0300

    Add some explanations related to heartbeat events

--- extensions/cdc-debezium/src/main/java/com/hazelcast/jet/cdc/ChangeEvent.java
+++ extensions/cdc-debezium/src/main/java/com/hazelcast/jet/cdc/ChangeEvent.java
@@ -22,23 +22,26 @@ import javax.annotation.Nonnull;
 
 /**
  * Information pertaining to a single data change event (insertion,
- * delete or update), affecting either a single database record
- * (in the case of relational databases) or document (in the case of
- * NoSQL databases).
+ * delete or update), affecting either a single database record (in the
+ * case of relational databases) or document (in the case of NoSQL
+ * databases).
  * <p>
- * Each event has a <i>key</i>, which identifies the particular
- * record or document being affected, and a <i>value</i>, which
- * describes the actual change itself.
+ * Each event has a <i>key</i>, which identifies the particular record
+ * or document being affected, and a <i>value</i>, which describes the
+ * actual change itself.
  * <p>
  * Most events have an <i>operation</i> associated with them which
  * specifies the type of change being described (insertion, delete or
  * update). This is really a property of the value, it's only replicated
  * at this level for convenience. Only some special events, like
- * heartbeats don't have an operation value.
+ * heartbeats don't have an operation value. (Heartbeat events are not
+ * something we encourage the usage of, but since no functionality of
+ * the underlying Debezium connectors is disabled they are still
+ * theoretically possible to enable and be observed in Jet.)
  * <p>
- * There is also a <i>timestamp</i> which specifies the moment in time when
- * the event happened. This timestamp is "real" in the sense that it
- * comes from the database change-log, so it's not "processing time",
+ * There is also a <i>timestamp</i> which specifies the moment in time
+ * when the event happened. This timestamp is "real" in the sense that
+ * it comes from the database change-log, so it's not "processing time",
  * it's not the moment when the event was observed by our system. Keep
  * in mind though that not all events come from the change-log. The
  * change-log goes back in time only to a limited extent, all older
