ddbfff4cc32bc456ab5c1dee235507e4d477a0ba
Bartok Jozsef <jozsi@hazelcast.com>
2020-05-26 10:42:38 +0300

    Address review concerns

--- extensions/cdc-debezium/src/main/java/com/hazelcast/jet/cdc/CdcSinks.java
+++ extensions/cdc-debezium/src/main/java/com/hazelcast/jet/cdc/CdcSinks.java
@@ -74,7 +74,7 @@ public final class CdcSinks {
      * <p>
      * Default value is 10 seconds.
      *
-     * @since 3.2
+     * @since 4.2
      */
     public static final HazelcastProperty SEQUENCE_CACHE_EXPIRATION_SECONDS
             = new HazelcastProperty("jet.cdc.sink.sequence.cache.expiration.seconds", 10, SECONDS);
@@ -83,10 +83,14 @@ public final class CdcSinks {
     }
 
     /**
-     * Returns a sink which maintains an up-to-date image of a change
-     * data capture stream in the form of an {@code IMap}. By image we
+     * Returns a sink which maintains an up-to-date mirror of a change
+     * data capture stream in the form of an {@code IMap}. By mirror we
      * mean that the map should always describe the end result of merging
      * all the change events seen so far.
+     * <p>
+     * <b>NOTE</b>: in order for the sink behaviour to be predictable
+     * the map should be non-existent or empty by the time the sink starts
+     * using it.
      * <p>
      * For each item the sink receives it uses the {@code keyFn} to
      * determine which map key the change event applies to. Then, based
@@ -124,9 +128,37 @@ public final class CdcSinks {
     }
 
     /**
-     * Convenience for {@link #map(String, FunctionEx, FunctionEx)} with
-     * actual {@code IMap} instance being passed in, instead of just
-     * name.
+     * Returns a sink which maintains an up-to-date mirror of a change
+     * data capture stream in the form of an {@code IMap}. By mirror we
+     * mean that the map should always describe the end result of merging
+     * all the change events seen so far.
+     * <p>
+     * <b>NOTE</b>: in order for the sink behaviour to be predictable
+     * the map should be non-existent or empty by the time the sink starts
+     * using it.
+     * <p>
+     * For each item the sink receives it uses the {@code keyFn} to
+     * determine which map key the change event applies to. Then, based
+     * on the {@code ChangeRecord}'s {@code Operation} it decides to
+     * either:
+     * <ul>
+     *   <li>delete the key from the map
+     *          ({@link Operation#DELETE})</li>
+     *   <li>insert a new value for the key
+     *          ({@link Operation#SYNC} & {@link Operation#INSERT})</li>
+     *   <li>update the current value for the key
+     *          ({@link Operation#UPDATE})</li>
+     * </ul>
+     * For insert and update operations the new value to use is
+     * determined from the input record by using the provided
+     * {@code valueFn}. <strong>IMPORTANT</strong> to note that if the
+     * {@code valueFn} returns {@code null}, then the key will be
+     * deleted from the map no matter the operation (ie. even for update
+     * and insert records).
+     * <p>
+     * For the functionality of this sink it is vital that the order of
+     * the input items is preserved so we'll always create a single
+     * instance of it in each pipeline.
      *
      * @since 4.2
      */
@@ -143,6 +175,9 @@ public final class CdcSinks {
      * Returns a sink equivalent to {@link #map}, but for a map in a
      * remote Hazelcast cluster identified by the supplied {@code
      * ClientConfig}.
+     * <p>
+     * <b>NOTE</b>: same limitation as for {@link #map}, the map should
+     * be non-existent or empty by the time the sink starts using it.
      * <p>
      * Due to the used API, the remote cluster must be at least version 4.0.
      *
@@ -228,37 +263,79 @@ public final class CdcSinks {
 
     }
 
+    /**
+     * Tracks the last seen sequence number for a set of keys. The
+     * sequence numbers originate from Debezium event headers and consist
+     * of two parts.
+     * <p>
+     * The <i>sequence</i> part is exactly what the name implies: a numeric
+     * sequence which we base our ordering on. Implementations needs to ensure
+     * that {@code ChangeRecord}s produced by a source contain a monotonic
+     * increasing sequence number, as long as the sequnce number partition
+     * doesn't change.
+     * <p>
+     * The <i>partition</i> part is a kind of context for the numeric
+     * sequence, the "source" of it if you will. It is necessary for avoiding
+     * the comparison of numeric sequences which come from different sources.
+     * For example if the numeric sequence is in fact based on transaction
+     * IDs, then it makes sense to compare them only if they are produced by
+     * the same database instance. Or if the numeric ID is an offset in a
+     * write-ahead log, then it makes sense to compare them only if they are
+     * offsets from the same log file. Implementations need to make sure that
+     * the partition is the same if and only if the source of the numeric
+     * sequence is the same.
+     * <p>
+     * Tracking of the sequence numbers for various keys happens in a
+     * LRU cache style, keys that haven't been updated nor read since a
+     * certain time (see {@code expirationMs} parameter) will be evicted,
+     * to keep the memory consumption limited. This is ok, because
+     * reordering of events happens only in case of events spaced very
+     * close to each other in time.
+     */
     private static final class Sequences<K> {
 
-        private static final int INITIAL_CAPACITY = 64 * 1024;
+        private static final int INITIAL_CAPACITY = 4 * 1024;
         private static final float LOAD_FACTOR = 0.75f;
 
         private final LinkedHashMap<K, long[]> sequences;
 
-        public Sequences(int expiration) {
+        /**
+         * @param expirationMs number of milliseconds for which a sequence
+         *                     observed for a certain key is guaranteed
+         *                     to be tracked; might be evicted afterwards
+         */
+        Sequences(int expirationMs) {
             sequences = new LinkedHashMap<K, long[]>(INITIAL_CAPACITY, LOAD_FACTOR, true) {
                 @Override
                 protected boolean removeEldestEntry(Map.Entry<K, long[]> eldest) {
                     long age = eldest.getValue()[2];
-                    return System.currentTimeMillis() - age > expiration;
+                    return System.currentTimeMillis() - age > expirationMs;
                 }
             };
         }
 
-        boolean update(K key, long timestamp, long partition, long value) {
+        /**
+         * @param key       key of an event that has just been observed
+         * @param timestamp timestamp of the source event
+         * @param partition partition of the source event sequence number
+         * @param sequence  numeric value of the source event sequence number
+         * @return true if the newly observed sequence number if more
+         * recent than what we have observed before (if any)
+         */
+        boolean update(K key, long timestamp, long partition, long sequence) {
             long[] prevSequence = sequences.get(key);
             if (prevSequence == null) { //first observed sequence for key
-                sequences.put(key, new long[] {partition, value, timestamp});
+                sequences.put(key, new long[]{partition, sequence, timestamp});
                 return true;
             } else {
                 prevSequence[2] = timestamp;
                 if (prevSequence[0] != partition) { //sequence partition changed for key
                     prevSequence[0] = partition;
-                    prevSequence[1] = value;
+                    prevSequence[1] = sequence;
                     return true;
                 } else {
-                    if (prevSequence[1] < value) { //sequence is newer than previous for key
-                        prevSequence[1] = value;
+                    if (prevSequence[1] < sequence) { //sequence is newer than previous for key
+                        prevSequence[1] = sequence;
                         return true;
                     } else {
                         return false;
