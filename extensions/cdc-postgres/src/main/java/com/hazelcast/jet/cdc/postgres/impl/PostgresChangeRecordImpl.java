492687e209e32667946412701b35d8498d94111f
Bartok Jozsef <jozsi@hazelcast.com>
2020-06-12 11:49:00 +0300

    Add "db" & "schema" fields

--- extensions/cdc-postgres/src/main/java/com/hazelcast/jet/cdc/postgres/impl/PostgresChangeRecordImpl.java
+++ extensions/cdc-postgres/src/main/java/com/hazelcast/jet/cdc/postgres/impl/PostgresChangeRecordImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2008-2020, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hazelcast.jet.cdc.postgres.impl;
 
 import com.hazelcast.jet.cdc.ParsingException;
@@ -29,5 +45,21 @@ public class PostgresChangeRecordImpl extends ChangeRecordImpl {
             databaseAndSchema = super.database() + "." + schema;
         }
         return databaseAndSchema;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode(); //extra field is recomputed on the fly
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        return super.equals(obj);
     }
 }
