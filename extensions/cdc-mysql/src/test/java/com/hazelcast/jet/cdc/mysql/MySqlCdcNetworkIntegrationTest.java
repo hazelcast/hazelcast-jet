0628bb6301001b5fc7230db8e363700eb78f3a81
Bartok Jozsef <jozsi@hazelcast.com>
2020-09-23 10:25:26 +0300

    Add comment to clarify intention

--- extensions/cdc-mysql/src/test/java/com/hazelcast/jet/cdc/mysql/MySqlCdcNetworkIntegrationTest.java
+++ extensions/cdc-mysql/src/test/java/com/hazelcast/jet/cdc/mysql/MySqlCdcNetworkIntegrationTest.java
@@ -101,6 +101,10 @@ public class MySqlCdcNetworkIntegrationTest extends AbstractCdcIntegrationTest {
 
     @Before
     public void before() {
+        //disable Testcontainer's automatic resource manager
+        //containers are cleaned up explicitly
+        //automatic resource manager is just an extra thing that can break
+        //(have had problems with it not being cleaned up properly itself)
         environmentVariables.set("TESTCONTAINERS_RYUK_DISABLED", "true");
         assertEquals("true", System.getenv("TESTCONTAINERS_RYUK_DISABLED"));
     }
