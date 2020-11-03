f185de455d31b436dbd9d8daf544707aeeb53b91
Viliam Durina <viliam-durina@users.noreply.github.com>
2020-11-03 17:07:34 +0100

    Remove ProjectJoinTransposeRule

--- hazelcast-jet-sql/src/main/java/com/hazelcast/jet/sql/impl/opt/logical/LogicalRules.java
+++ hazelcast-jet-sql/src/main/java/com/hazelcast/jet/sql/impl/opt/logical/LogicalRules.java
@@ -44,7 +44,6 @@ public final class LogicalRules {
                 ProjectMergeRule.INSTANCE,
                 ProjectRemoveRule.INSTANCE,
                 ProjectFilterTransposeRule.INSTANCE,
-                ProjectJoinTransposeRule.INSTANCE,
                 ProjectIntoScanLogicalRule.INSTANCE,
 
                 // Scan rules
