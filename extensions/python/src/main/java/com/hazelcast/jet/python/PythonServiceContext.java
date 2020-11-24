9906b1d395f3b641c4869867d2a8d70f841eaf9e
Ali Gurbuz <ali@hazelcast.com>
2020-11-24 12:42:34 +0300

    checkstyle

--- extensions/python/src/main/java/com/hazelcast/jet/python/PythonServiceContext.java
+++ extensions/python/src/main/java/com/hazelcast/jet/python/PythonServiceContext.java
@@ -29,7 +29,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.PosixFilePermission;
